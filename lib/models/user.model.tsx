865f594e521b31bfd0540ae1c76483b9f37d8172
louai <louaibaghdadi27@gmail.com>
2024-07-24 15:20:16 +0300

    second commit

--- lib/models/user.model.tsx
+++ lib/models/user.model.tsx
@@ -3,18 +3,19 @@ import { Schema } from "mongoose";
 
 const userSchema = new mongoose.Schema({
     clerkId: { type: String, required: true, unique: true },
-    name: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     email: { type: String, required: true },
     image: { type: String, required: true },
-    budgets: [{
-        type: Schema.Types.ObjectId, ref: "Budget"
-    }],
-    incomes: [{
-        type: Schema.Types.ObjectId, ref: "Income"
-    }],
-    expenses: [{
-        type: Schema.Types.ObjectId, ref: "Expense"
-    }],
+    // budgets: [{
+    //     type: Schema.Types.ObjectId, ref: "Budget"
+    // }],
+    // incomes: [{
+    //     type: Schema.Types.ObjectId, ref: "Income"
+    // }],
+    // expenses: [{
+    //     type: Schema.Types.ObjectId, ref: "Expense"
+    // }],
 });
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
